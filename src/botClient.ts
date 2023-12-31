d64744b0454ca8b7f8e774ecb8640db0b79fda36
Bryan Scott <bryan@pajato.com>
2023-12-30 19:09:38 -0500

    fix: split function properly orders splits

--- src/botClient.ts
+++ src/botClient.ts
@@ -168,15 +168,18 @@ export class BotClient {
     }
 
     private split(response: string): string[] {
-        const responses: string[] = response.split("\n")
-        const splitResponse: string[] = [""]
+        const lines: string[] = response.split("\n")
+        const splitResponse: string[] = []
 
-        responses.forEach(response => {
-            const index = splitResponse.findIndex((entry) => (entry.length + response.length) < 1024)
-            if (index < 0) {
+        let currentIndex = 0
+        lines.forEach((response) => {
+            if (splitResponse.length <= currentIndex) {
                 splitResponse.push(response)
+            } else if (splitResponse[currentIndex].length + response.length < 2000) {
+                splitResponse[currentIndex] = splitResponse[currentIndex] + "\n" + response
             } else {
-                splitResponse[index] = splitResponse[index] + "\n" + response
+                splitResponse.push(response)
+                currentIndex += 1
             }
         })
         return splitResponse
